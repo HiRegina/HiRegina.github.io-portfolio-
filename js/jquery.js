b528cfbc7a8fdf359d447f3508c7d0534751a282
HiRegina <jokerching101@gmail.com>
2021-05-18 18:16:11 +0800

    update

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,6 @@
 document.write( '<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>\n' );
 document.write( '<script type=\"text/javascript\">\n' );
 document.write( '    $(document).ready(function(){\n' );
-document.write( '    $(\'#header\').load(\'hiregina.github.io/HiRegina.github.io-portfolio-/header.html\'); \n' );
+document.write( '    $(\'#header\').load(\'https://hiregina.github.io/HiRegina.github.io-portfolio-/header.html\'); \n' );
 document.write( '}); \n' );
 document.write( '</script>' );
