ab78a8f4044486f0b9eedc617fd6401312e72046
HiRegina <jokerching101@gmail.com>
2021-05-18 18:41:23 +0800

    footer

--- js/jquery.js
+++ js/jquery.js
@@ -2,6 +2,5 @@ document.write( '<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.
 document.write( '<script type=\"text/javascript\">\n' );
 document.write( '    $(document).ready(function(){\n' );
 document.write( '    $(\'#header\').load(\'https://hiregina.github.io/HiRegina.github.io-portfolio-/header.html\'); \n' );
-document.write( '    $(\'#footer\').load(\'https://hiregina.github.io/HiRegina.github.io-portfolio-/footer.html\'); \n' );
 document.write( '}); \n' );
 document.write( '</script>' );
